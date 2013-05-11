6fbc2e341affd0be3f69d4ccb770d53c7f7184bb
fastpoke <fastpoke@fastpoke.org>
2013-05-12 03:41:35 +0700

    Powered by 十六夜 咲夜 and レミリア・スカーレット

---
+++ Chapter 4/4.1B/4.1B.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct gensokyo {
+	char name[40];
+	int dd, mm, yy;
+	char *proff;
+	gensokyo *next;
+};
+
+gensokyo Sakuya = {"十六夜 咲夜", 1, 1, 1000, "maid", NULL};
+gensokyo Remilia = {"レミリア・スカーレット", 9, 9, 1000, "scarlet devil", &Sakuya};
+gensokyo *p = &Remilia;
+
+void someFunction() {
+	char c1, c2, c3, c4;
+	c1 = Sakuya.name[2];
+	c2 = Remilia.proff[3];
+	c3 = p->name[3];
+	c4 = p->next->proff[1];
+	std::cout << std::endl << "Powered by " << Sakuya.name << " and " << Remilia.name << std::endl;
+	std::cout << c1 << c2 << c3 << c4 << std::endl << std::endl;
+}
+
+int main() {
+	someFunction();
+	return 0;
+}
